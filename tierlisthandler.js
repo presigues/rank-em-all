64c71eb1bcc44fbb17c9865b6f0c89e9d63219a6
presigues <132411345+presigues@users.noreply.github.com>
2024-07-18 19:47:49 -0400

    debug / style

--- tierlisthandler.js
+++ tierlisthandler.js
@@ -67,34 +67,42 @@ switch(bluebg1){
 
   case 1:
     document.getElementById("b1").setAttribute("src", "https://www.youtube.com/embed/QyPR77rg1to?list=PLpJl5XaLHtLX-pDk4kctGxtF4nq6BIyjg");
+    document.getElementById("text1").innerHTML = "Ruins - Toby Fox - Undertale OST";
     // ruins - UOST
     break;
     case 2:
       document.getElementById("b1").setAttribute("src", "https://www.youtube.com/embed/4w3VqzwJ1j4?list=PL8C3D47E6FA9CDDC4");
+      document.getElementById("text1").innerHTML = "Main Menu -  Laura Shigihara - Plants Vs Zombies OST";
       // main menu - PvZost--
     break;
     case 3:
       document.getElementById("b1").setAttribute("src", "https://www.youtube.com/embed/uQHf9T_9Uno?list=PL72BLeZJTSROUYz97aZdVdl9590M7qZD");
+      document.getElementById("text1").innerHTML = "Main Screen -  Kazumi Totaka - Wii Sports OST";
       // main screen - wii sports ost
       break;
       case 4:
         document.getElementById("b1").setAttribute("src", "https://www.youtube.com/embed/8gkTtNefxws?list=PLEUKcNuP7bDX9RoW3HqYR6EFvWZh12upZ");
+        document.getElementById("text1").innerHTML = "THE WORLD REVOLVING - Toby Fox - Deltarune OST";
         // the world is revolving - DOST
         break;
         case 5:
           document.getElementById("b1").setAttribute("src", "https://www.youtube.com/embed/IJjYlFAFnoA?list=PLoqOgJJaN57YiiY-JrdSvBVCprMPednXl");
+          document.getElementById("text1").innerHTML = "Overture - Kazumi Totaka - Super Mario Galaxy OST";
           //overture - SMGOST
           break;
           case 6:
             document.getElementById("b1").setAttribute("src", "https://www.youtube.com/embed/hMa4hZQbrms?list=PLpJl5XaLHtLX-pDk4kctGxtF4nq6BIyjg");
+            document.getElementById("text1").innerHTML = "ASGORE - Toby Fox - Undertale OST";
             // asgore - UOST
             break;
             case 7:
               document.getElementById("b1").setAttribute("src", "https://www.youtube.com/embed/qq-RGFyaq0U?list=PLefKpFQ8Pvy5aCLAGHD8Zmzsdljos-t2l");
-              // minecraft - MVA
+              document.getElementById("text1").innerHTML = "Minecraft - C418 - Minecraft OST";
+                            // minecraft - MVA
               break;
               case 8:
                 document.getElementById("b1").setAttribute("src",  "https://www.youtube.com/embed/-8p8VowCmgE?list=PLEUKcNuP7bDX9RoW3HqYR6EFvWZh12upZ");
+                document.getElementById("text1").innerHTML = "BIG SHOT - Toby Fox - Deltarune OST";
                 // big shot - DOST
                 break;
 
@@ -104,34 +112,42 @@ switch(bluebg2){
 
   case 1:
     document.getElementById("b2").setAttribute("src", "https://www.youtube.com/embed/QyPR77rg1to?list=PLpJl5XaLHtLX-pDk4kctGxtF4nq6BIyjg");
+    document.getElementById("text2").innerHTML = "Ruins - Toby Fox - Undertale OST";
     // ruins - UOST
     break;
     case 2:
       document.getElementById("b2").setAttribute("src", "https://www.youtube.com/embed/4w3VqzwJ1j4?list=PL8C3D47E6FA9CDDC4");
+      document.getElementById("text2").innerHTML = "Main Menu -  Laura Shigihara - Plants Vs Zombies OST";
       // main menu - PvZost--
     break;
     case 3:
-      document.getElementById("b2").setAttribute("src", "https://www.youtube.com/embed/uQHf9T_9Uno?list=PL72BLeZJTSROUYz97aZdVdl9590M7qZD");
+      document.getElementById("b2").setAttribute("src", "https://www.youtube.com/embed/uQHf9T_9Uno?list=PL72BLeZJTSROUYz97aZdVdl9590M7qZD-");
+      document.getElementById("text2").innerHTML = "Main Screen -  Kazumi Totaka - Wii Sports OST";
       // main screen - wii sports ost
       break;
       case 4:
         document.getElementById("b2").setAttribute("src", "https://www.youtube.com/embed/8gkTtNefxws?list=PLEUKcNuP7bDX9RoW3HqYR6EFvWZh12upZ");
+        document.getElementById("text2").innerHTML = "THE WORLD REVOLVING - Toby Fox - Deltarune OST";
         // the world is revolving - DOST
         break;
         case 5:
           document.getElementById("b2").setAttribute("src", "https://www.youtube.com/embed/IJjYlFAFnoA?list=PLoqOgJJaN57YiiY-JrdSvBVCprMPednXl");
+          document.getElementById("text2").innerHTML = "Overture - Kazumi Totaka - Super Mario Galaxy OST";
           //overture - SMGOST
           break;
           case 6:
             document.getElementById("b2").setAttribute("src", "https://www.youtube.com/embed/hMa4hZQbrms?list=PLpJl5XaLHtLX-pDk4kctGxtF4nq6BIyjg");
+            document.getElementById("text2").innerHTML = "ASGORE - Toby Fox - Undertale OST";
             // asgore - UOST
             break;
             case 7:
               document.getElementById("b2").setAttribute("src", "https://www.youtube.com/embed/qq-RGFyaq0U?list=PLefKpFQ8Pvy5aCLAGHD8Zmzsdljos-t2l");
+              document.getElementById("text2").innerHTML = "Minecraft - C418 - Minecraft OST";
               // minecraft - MVA
               break;
               case 8:
                 document.getElementById("b2").setAttribute("src",  "https://www.youtube.com/embed/-8p8VowCmgE?list=PLEUKcNuP7bDX9RoW3HqYR6EFvWZh12upZ");
+                document.getElementById("text2").innerHTML = "BIG SHOT - Toby Fox - Deltarune OST";
                 // big shot - DOST
                 break;
 
@@ -144,7 +160,8 @@ switch(bluebg2){
   document.getElementById("howto2").style.display = "none";
   document.getElementById("letsgo").style.display = "none";
 
-  document.getElementById("cat").style.top = "550px"
-  document.getElementById("cat").style.left = "20px"
+  document.getElementById("cat").style.top = "50px"
+  document.getElementById("cat").style.left = "10px"
   document.getElementById("one").style.display = "block";
+  document.getElementById("two").style.display = "block";
  }
